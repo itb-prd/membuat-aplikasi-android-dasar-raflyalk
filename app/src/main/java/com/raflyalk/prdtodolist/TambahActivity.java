b7b54f1978b49f0667619e1b4838fcf3fdc8b244
raflyalk <raflyalk@gmail.com>
2016-11-20 16:54:55 +0700

    tes lagi

--- app/src/main/java/com/raflyalk/prdtodolist/TambahActivity.java
+++ app/src/main/java/com/raflyalk/prdtodolist/TambahActivity.java
@@ -15,19 +15,6 @@ public class TambahActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah);
-
-        Button tombol = (Button) findViewById(R.id.button);
-        tombol.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent goBack = new Intent();
-                EditText editText = (EditText) findViewById(R.id.tulisToDo);
-                String message = String.valueOf(editText.getText());
-                goBack.putExtra("tambah_konten", message);
-                setResult(RESULT_OK, goBack);
-                finish();
-            }
-        });
     }
 
     public void sendNewItem(View view){
