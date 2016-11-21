9842fba695642a53b3c80deb68914e2b5fbc9e3c
raflyalk <raflyalk@gmail.com>
2016-11-21 22:56:27 +0700

    COMPLETE v1.0

--- app/src/main/java/com/raflyalk/prdtodolist/editScreen.java
+++ app/src/main/java/com/raflyalk/prdtodolist/editScreen.java
@@ -1,13 +1,10 @@
 package com.raflyalk.prdtodolist;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class editScreen extends AppCompatActivity {
 
